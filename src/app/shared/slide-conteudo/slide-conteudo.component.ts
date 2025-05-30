84a1c884f4955735be846d206cb2fcf0dd1eb977
Kevin Silva <kevinmarcos280@gmail.com>
2025-05-30 12:32:52 -0300

    ajustando fullscreen

--- src/app/shared/slide-conteudo/slide-conteudo.component.ts
+++ src/app/shared/slide-conteudo/slide-conteudo.component.ts
@@ -15,6 +15,7 @@ export class SlideConteudoComponent {
   @Output() menuToggle = new EventEmitter<boolean>();
 
   menuOculto = false;
+  fullscreenAtivo = false;
 
   onSlideChange(event: any) {
     const activeSlideId = parseInt(event.current.replace('ngb-slide-', ''), 10);
@@ -29,10 +30,38 @@ export class SlideConteudoComponent {
     this.menuOculto = !this.menuOculto;
     this.menuToggle.emit(this.menuOculto);
   }
+
+  toggleFullscreen() {
+    this.fullscreenAtivo = !this.fullscreenAtivo;
+
+
+    const elem = document.documentElement;
+
+    if (this.fullscreenAtivo) {
+      // Entra no modo tela cheia do navegador
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if ((elem as any).webkitRequestFullscreen) {
+        (elem as any).webkitRequestFullscreen(); // Safari
+      } else if ((elem as any).msRequestFullscreen) {
+        (elem as any).msRequestFullscreen(); // IE11
+      }
+    } else {
+      // Sai do modo tela cheia
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else if ((document as any).webkitExitFullscreen) {
+        (document as any).webkitExitFullscreen(); // Safari
+      } else if ((document as any).msExitFullscreen) {
+        (document as any).msExitFullscreen(); // IE11
+      }
+    }
+
+  }
 }
 
 export interface CarouselItem {
   type: 'image' | 'video';
   src: string;
-  
+
 }
