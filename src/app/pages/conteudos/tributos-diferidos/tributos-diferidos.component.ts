47bb277f9279b3ef7f4ed80c04c2cc0021a247e1
Kevin Silva <kevinmarcos280@gmail.com>
2025-07-07 14:36:48 -0300

    ajsute

--- src/app/pages/conteudos/tributos-diferidos/tributos-diferidos.component.ts
+++ src/app/pages/conteudos/tributos-diferidos/tributos-diferidos.component.ts
@@ -17,16 +17,16 @@ export class TributosDiferidosComponent {
     menuOculto = false;
   
     carouselItems: CarouselItem[] = [
-      { type: 'video', src: '/assets/tributos-diferidos/1.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/2.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/3.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/4.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/5.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/6.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/7.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/8.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/9.mp4' },
-      { type: 'video', src: '/assets/tributos-diferidos/10.mp4'},
+      { type: 'video', src: '/assets/videos/tributos-diferidos/1.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/2.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/3.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/4.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/5.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/6.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/7.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/8.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/9.mp4' },
+      { type: 'video', src: '/assets/videos/tributos-diferidos/10.mp4'},
     ];
   
     ngOnInit() {
